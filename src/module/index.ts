8a2e8cfe91f36b1b39629bebcad77d17ccec205f
Ebrahim khasal <webtanan@gmail.com>
2026-09-05 17:57:10 +0330

    alpha: build Tiny Weekly Review standalone and module (#1)

---
+++ src/module/index.ts
@@ -0,0 +1,1 @@
+export{moduleManifest}from'../../module.manifest';export{WeeklyReviewWorkspace,type WeeklyReviewWorkspaceProps}from'../components/WeeklyReviewWorkspace';export{buildWeeklyReview}from'../domain/review';export type{WeeklySignals,WeeklyReview}from'../domain/review';
