a2e758654662c2c060a37f68f4a154223fddcb2a
Ebrahim khasal <webtanan@gmail.com>
2026-09-06 11:37:43 +0330

    fix(package): emit Weekly Review module CSS

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,1 +1,6 @@
-export{moduleManifest}from'../../module.manifest';export{WeeklyReviewWorkspace,type WeeklyReviewWorkspaceProps}from'../components/WeeklyReviewWorkspace';export{buildWeeklyReview}from'../domain/review';export type{WeeklySignals,WeeklyReview}from'../domain/review';
+import '../styles.css';
+
+export { moduleManifest } from '../../module.manifest';
+export { WeeklyReviewWorkspace, type WeeklyReviewWorkspaceProps } from '../components/WeeklyReviewWorkspace';
+export { buildWeeklyReview } from '../domain/review';
+export type { WeeklySignals, WeeklyReview } from '../domain/review';
